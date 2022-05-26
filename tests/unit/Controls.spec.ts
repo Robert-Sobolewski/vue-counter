340b10e922c1968a98075a1f592a3e11c0bc54a3
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-05-26 15:15:46 +0200

    last commit

--- tests/unit/Controls.spec.ts
+++ tests/unit/Controls.spec.ts
@@ -1,7 +1,10 @@
-import { mount } from "@vue/test-utils"
+import { flushPromises, mount } from "@vue/test-utils"
+import axios from "axios"
 import Controls from '../../src/components/Controls.vue'
 
 describe('testing Controls component', () => {
+
+    jest.spyOn(axios,'get').mockResolvedValue({data:9})
 
     test('rendering component', () => {
         const wrapper = mount(Controls,{})
@@ -12,17 +15,28 @@ describe('testing Controls component', () => {
         const wrapper = mount(Controls,{})
 
         const addBtn = wrapper.find('[data-testid="addBtn"]')
-        const subBtn = wrapper.find('[data-testid="subBtn"]')
-        const rndBtn = wrapper.find('[data-testid="rndBtn"]')
+       
        await addBtn.trigger('click')
         expect(wrapper.emitted()).toHaveProperty('add')
         expect(wrapper.emitted('add')![0]).toEqual([1])
-
+    })
+    test('emit sub event', async() => {
+        const wrapper = mount(Controls,{})
+        const subBtn = wrapper.find('[data-testid="subBtn"]')
         await subBtn.trigger('click')
         expect(wrapper.emitted()).toHaveProperty('add')
-        expect(wrapper.emitted('add')![1]).toEqual([-1])
+        expect(wrapper.emitted('add')![0]).toEqual([-1])
 
-        await rndBtn.trigger('click')
-        expect(wrapper.emitted()).toHaveProperty('add')
+       
     })
+    // test('emit rnd event', async() => {
+    //     const wrapper = mount(Controls,{})
+
+    //     const rndBtn = wrapper.find('[data-testid="rndBtn"]')
+      
+    //     await rndBtn.trigger('click')
+    //     await flushPromises()
+    //     expect(wrapper.emitted()).toHaveProperty('add')
+    //     expect(wrapper.emitted('add')![0]).toEqual([9])
+    // })
 })
